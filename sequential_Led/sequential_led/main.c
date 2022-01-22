68f17d7f286a51d0dcf8ac4c902f37fc0f1ba50a
soy567 <37576447+soy567@users.noreply.github.com>
2022-01-22 19:17:54 +0900

    Update main.c

--- sequential_Led/sequential_led/main.c
+++ sequential_Led/sequential_led/main.c
@@ -4,35 +4,35 @@
 #include "Button.h"
 #include "Led.h"
 
-enum {eLeftShitf, eRightShift};
+enum {eLeftShitf, eRightShift};			// enum 이용하여 프로그램이 가질 수 있는 상태 정의
 
 int main(void)
 {
-    uint8_t ledstateMachine = eLeftShitf;
+    uint8_t ledstateMachine = eLeftShitf;	// main의 상태 저장할 변수
 	
-	// 초기화 코드
-	initLed();
-	buttonInit();
-	setLedData(0x01); 
+    // 초기화 코드
+    initLed();
+    buttonInit();
+    setLedData(0x01); 
 	
     while (1) 
     {
-		switch(ledstateMachine) {
-			case eLeftShitf:
-			leftShift();
-			if (getButton1State()) {
-				ledstateMachine = eRightShift;
-			}
-			break;
-			
-			case eRightShift:
-			rightShift();
-			if (getButton1State()) {
-				ledstateMachine = eLeftShitf;
-			}
-			break;
+	switch(ledstateMachine) {	// switch문 사용하여 상태 구분, 실행 로직 정의
+	    case eLeftShitf:		
+	        leftShift();
+		if (getButton1State()) {	// 버튼 이용하여 다른 상태로 전환
+		    ledstateMachine = eRightShift;
 		}
-		_delay_ms(100);
+		break;
+			
+	    case eRightShift:
+		rightShift();
+		if (getButton1State()) {
+		    ledstateMachine = eLeftShitf;
+		}
+		break;
+	}
+	_delay_ms(100);		// 100ms 딜레이
     }
 }
 
