839533d23b1105d66ec903f2e8b22ab9ba3205e1
soy567 <37576447+soy567@users.noreply.github.com>
2022-01-22 19:12:32 +0900

    Update Led.c

--- sequential_Led/sequential_led/Led.c
+++ sequential_Led/sequential_led/Led.c
@@ -4,26 +4,31 @@
 
 uint8_t ledData;  // led bar 의 속성 저장할 변수
 
+// LED Driver 내부 변수 Setter
 void setLedData(uint8_t data) {
-    if (data > 0xff)
-        data = 0xff;
-    else if (data < 0)
-        data = 0;
-    ledData = data;
+    if (data > 0xff)    // 범위 초과한 경우
+        data = 0xff;    // 모든 LED 켜기
+    else if (data < 0)  // 최솟값 이하인 경우
+        data = 0;       // 모든 LED 끄기
+    ledData = data;     // 입력받은 데이터 ledData에 저장
 }
 
-uint8_t getLedData() {
+// LED Driver 내부 변수 Getter
+uint8_t getLedData() {  
     return ledData;
 }
 
+// LED 연결된 PORT 방향 Register 모두 출력으로 설정
 void ledbar() {
     LED_DDR = 0xff;
 }
 
+// LED 연결된 PORT(8-output)에 데이터 입력하여 원하는 포트만 High 출력
 void writeLedData(uint8_t data) {
     LED_PORT = data;
 }
 
+// LED 초기화
 void initLed() {
     // LED_DDR = 0xff;
     LED_DDR |= (1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4) | (1 << LED5) | (1 << LED6) | (1 << LED7) | (1 << LED8);
